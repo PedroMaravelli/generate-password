849c2211255dd85012d6151300b7a20830af329e
PedroMaravelli <phmaravelli123@gmail.com>
2024-05-28 15:00:16 -0300

    feauture: screen generate password finally

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,10 +1,62 @@
-import { CardContainer, GenerateContainer } from "./style";
+import { CardContainer, GenerateContainer, HeaderContainer, InputNumbers, Inputs, InputsForm, LeftAndRightCardContainer, LeftContentCard, RightContentCard } from "./style";
 
 export function GeneratePassword(){
     return(
         <GenerateContainer>
             <CardContainer>
-                <h1>Generate Password</h1>
+                
+                    <h1>Gerador de Senhas </h1>
+                    <article></article>
+                
+                <LeftAndRightCardContainer>
+
+                
+                    <LeftContentCard>
+                        <InputsForm>
+                            <label htmlFor="lenght">Tamanho</label>
+                            <input type="number" min={3} max={20} name="lenght" required id="lenght" />
+                        </InputsForm>
+
+                        <InputsForm>
+                            <label htmlFor="number">Números</label>
+                            <InputNumbers>
+                                <Inputs type="button" name="number" id="number" value="Sim" />
+                                <Inputs type="button" name="number" id="number" value="Não" />
+                            </InputNumbers>
+
+                        </InputsForm>
+                        
+
+                    </LeftContentCard>
+                    <LeftContentCard>
+                        <InputsForm>
+                            <label htmlFor="simbolos">Caracteres Especiais </label>
+                            <InputNumbers>
+                                <Inputs type="button" name="simbolos" id="simbolos" value="Sim" />
+                                <Inputs type="button" name="simbolos" id="simbolos" value="Não" />
+                        </InputNumbers>
+
+                        </InputsForm>
+
+                        <InputsForm>
+                            <label htmlFor="uppercase">Letras Maiúsculas</label>
+                            <InputNumbers>
+                                <Inputs type="button" name="uppercase" id="uppercase" value="Sim" />
+                                <Inputs type="button" name="uppercase" id="uppercase" value="Não" />
+                            </InputNumbers>
+
+                        </InputsForm>
+                    </LeftContentCard>
+
+                    <RightContentCard>
+                        <p>Senha</p>
+                        <input type="text"/>
+                        <button type="submit">Gerar Senha </button>
+                    </RightContentCard>
+                </LeftAndRightCardContainer>
+
+        
+                
             </CardContainer>
 
         </GenerateContainer>
--- src/screens/style.ts
+++ src/screens/style.ts
@@ -8,16 +8,129 @@ export const GenerateContainer = styled.div`
     width: 100%;
     height: 100vh;
 `
-export const CardContainer = styled.div`
+export const CardContainer = styled.form`
     background-color:#474747 ;
-    width: 50%;
+    width: 70%;
     border-radius: 10px;
+    padding-bottom: 4rem;
+    display: flex;
+    flex-direction: column;
 
     h1{
         color: white;
         text-align: center;
         font-size:1.8rem;
+        margin-top: 2rem;
+    }
+    article{
+        margin: 0 auto;
+        border-bottom: 1px solid gray;
+        width: 90%;
         margin-top: 1rem;
+        margin-bottom: 1rem ;
+    }
+
+`
+export const LeftAndRightCardContainer = styled.div`
+    display: flex;
+    width: 100%;
+
+`
+
+export const LeftContentCard = styled.div`
+    display: flex;
+    width: 50%;
+    flex-wrap: wrap;
+
+`
+export const InputsForm = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    color: white;
+    margin-top: 3rem;
+    margin-left: 5rem;
+    text-align: center;
+    gap: 0.5rem;
+    
+
+    label{
+        font-size: 1.4rem;
+        font-weight: bold ;
+        
+    }
+
+    input{
+        width: 100%;
+        height: 2rem;
+        border-radius: 5px;
+        background-color: gray;
+        border: none;
+        font-size: 1rem;
+        font-weight:600;
+        color: white;
+        padding-left: 1rem;
+    
+    }
+
+
+`
+export const InputNumbers = styled.div`
+    display: flex;
+    gap: 0.5rem;
+
+`
+export const Inputs = styled.input`
+    color: white;
+
+    &:hover{
+        opacity: 0.8;
+    }
+    &:focus{
+        opacity: 0.6;
+    }
+
+`
+export const RightContentCard = styled.div`
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 3rem;
+
+    p{
+        color: white;
+        font-size: 1.3rem;
+        margin-bottom: 0.6rem;
+        font-weight: bold;
+    }
+    input{
+        width: 70%;
+        background-color: gray;
+        border-radius: 5px;
+        border: none;
+        color: white;
+        text-align: center;
+        height: 2rem;
+        padding-top: 0.2rem;
+        margin-bottom: 3rem;
+    }
+
+    button{
+        width: 50%;
+        height: 2.2rem;
+        border-radius: 5px;
+        border: none;
+        background-color:#3FCF4E ;
+        color: white;
+        font-size: 1.2rem;
+        font-weight: bold;
+
+
+        &:hover{
+            opacity: 0.8;
+        }
     }
 
 `
